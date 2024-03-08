d2e4ac1694ba5bbac40b7cf37467a5afc9c40339
teosoph <teosoph@gmail.com>
2024-03-08 11:17:54 +0200

    add__HW-2

---
+++ HomeWork-2/scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log("Deployer address:", deployer.address);
+
+    const SimpleMessage = await hre.ethers.getContractFactory("SimpleMessage");
+    const simpleMessage = await SimpleMessage.deploy("Ihor Kyrychenko, 07.03.2024");
+
+    await simpleMessage.deployed();
+
+    console.log("SimpleMessage deployed to:", simpleMessage.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
